c53b22b7c095ac683c6edcd302d3d0053df0a160
ttk0721 <88659251+ttk0721@users.noreply.github.com>
2021-08-21 12:28:13 +0200

    qprowadzono poprawki do notatnika

--- szybki_notatnik.Designer.cs
+++ szybki_notatnik.Designer.cs
@@ -37,6 +37,7 @@ namespace moja_baza
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -52,7 +53,7 @@ namespace moja_baza
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(2, 12);
+            this.textBox1.Location = new System.Drawing.Point(1, 87);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(798, 31);
             this.textBox1.TabIndex = 1;
@@ -60,7 +61,7 @@ namespace moja_baza
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox2.Location = new System.Drawing.Point(2, 72);
+            this.textBox2.Location = new System.Drawing.Point(1, 134);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(798, 31);
             this.textBox2.TabIndex = 2;
@@ -76,7 +77,7 @@ namespace moja_baza
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox4.Location = new System.Drawing.Point(2, 341);
+            this.textBox4.Location = new System.Drawing.Point(1, 356);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(798, 31);
             this.textBox4.TabIndex = 4;
@@ -84,7 +85,7 @@ namespace moja_baza
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox5.Location = new System.Drawing.Point(2, 134);
+            this.textBox5.Location = new System.Drawing.Point(1, 186);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(798, 31);
             this.textBox5.TabIndex = 5;
@@ -92,7 +93,7 @@ namespace moja_baza
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox6.Location = new System.Drawing.Point(1, 271);
+            this.textBox6.Location = new System.Drawing.Point(1, 300);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(798, 31);
             this.textBox6.TabIndex = 6;
@@ -100,16 +101,27 @@ namespace moja_baza
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox7.Location = new System.Drawing.Point(1, 210);
+            this.textBox7.Location = new System.Drawing.Point(1, 243);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(798, 31);
             this.textBox7.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(776, 44);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "zapisz i wyjdź";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // szybki_notatnik
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -138,5 +150,6 @@ namespace moja_baza
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button1;
     }
 }
