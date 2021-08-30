10507f29528ede5db4881daa054d91b9f7e16ea7
ttk0721 <88659251+ttk0721@users.noreply.github.com>
2021-08-30 18:52:47 +0200

    nie udany system przerzucania storn (na git idzie do sprawdzenia)

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,29 @@ namespace moja_baza
         public Form1()
         {
             InitializeComponent();
+            private void button1_Click(object sender, EventArgs e)
+            {
+                timer1.Start();
+                Form2 Form2 = new Form2();
+                Form2.ShowDialog();
+            }
+
+            private void timer1_Tick(object sender, EventArgs e)
+            {
+                label3.Text = "Window 1 will close in " + ((float)time1 / 10).ToString() + " seconds";
+                time1--;
+                if (time1 == 0)
+                {
+                    Program.interrupt = false;
+                    this.Close(); //Close Form1 Form
+                }
+            }
+
+            private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                if (Program.interrupt)
+                    Application.Exit();  //Close application on click window close
+            }
         }
     }
 }
