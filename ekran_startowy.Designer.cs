83fdb70c06a9504872e01a0c59bd047ba70c4094
ttk0721 <88659251+ttk0721@users.noreply.github.com>
2021-09-10 18:32:21 +0200

    dodano opisy i zaktualizowano wersję

--- ekran_startowy.Designer.cs
+++ ekran_startowy.Designer.cs
@@ -78,6 +78,7 @@ namespace moja_baza
             this.button3.TabIndex = 3;
             this.button3.Text = "inna opcja";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label2
             // 
--- ekran_startowy.cs
+++ ekran_startowy.cs
@@ -18,18 +18,23 @@ namespace moja_baza
 
         }
 
-        private void button2_Click_1(object sender, EventArgs e)
+        private void button2_Click_1(object sender, EventArgs e)// program dla drugiego przycisku na ekranie starowym. (okienko informacyjne)
         {
-            MessageBox.Show("moja_baza\n Wersja 1.1.0\n(c) działają przyciski na ekranie głównym");
+            MessageBox.Show("moja_baza\n Wersja 1.1.1.1\n(c) dodano nazwy");
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)// tu jest miejse na prograam pierwszego przycisku od lewej na ekranie startowym.(prejście do szybkiego notatnika)
         {
             this.SetVisibleCore(false); //SP5IOU Hide login form
             szybki_notatnik Form2 = new szybki_notatnik();
             Form2.ShowDialog();
             this.SetVisibleCore(true); //SP5IOU Show login form again
                                        //               this.Close(); //SP5IOU Missing closing form command
+        }
+
+        private void button3_Click(object sender, EventArgs e)// tu jest miejse na prograam trzeciego przycisku od lewej na ekranie startowym.
+        {
+
         }
     }
 }
